419275809f668c7f8862d37ff6d3bf24614b9416
Jamal <jamalmendezfeliz@hotmail.com>
2025-05-25 01:11:47 -0400

    Creacion del view de posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -15,6 +19,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    objects = models.Manager
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
